885249494ef6c6ed228f0cbe15fc63fa0018b296
unknown6656 <unknown84@gmx.de>
2021-02-22 15:24:28 +0100

    initial code commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GPSReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
