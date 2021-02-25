43ca5532a1ec34b8b81fa03d17d1b12fdf7522e5
unknown6656 <unknown84@gmx.de>
2021-02-25 13:22:51 +0100

    updated test program

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ using System;
 
 using GPSSensor gps = new();
 
+gps.OnValidDataReceived += (_, d) =>
+{
+    if (d is NMEA0183Data.GNSSData { Coordinates: var coord })
+        Console.WriteLine(coord);
+};
 gps.Start("COM7");
 
 
